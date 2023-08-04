6c3520953c862cf606e931fdf622ee56000eca48
art3mes <93867313+art3mes@users.noreply.github.com>
2023-08-04 22:28:59 +0530

    header + background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
   
   return (
     <div>
-      yes
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return (
+        <div id="header">
+            <div className="header-left">Put text</div>
+        </div>
+    );
+}
+
+export default Header;
