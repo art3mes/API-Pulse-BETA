270a9b3956fbc0e00973ea46d72496cdb6c81b9e
art3mes <93867313+art3mes@users.noreply.github.com>
2023-08-04 22:42:30 +0530

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <div id="footer">
+            <div className="footer-left">Put text</div>
+        </div>
+    );
+}
+
+export default Footer;
