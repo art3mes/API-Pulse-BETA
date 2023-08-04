21586a958e3eb00a8288c0b3ad9943a5b4a4c0e1
art3mes <93867313+art3mes@users.noreply.github.com>
2023-08-04 23:17:24 +0530

    grid body

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Grid from "./Grid";
 
 function App() {
   
   return (
     <div>
       <Header />
+      <Grid />
       <Footer />
     </div>
   );
---
+++ src/components/Grid.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Grid(){
+    return (
+        <div className="main-grid">
+            <div className="grid-item">One</div>
+            <div className="grid-item">Two</div>
+            <div className="grid-item">Three</div>
+            <div className="grid-item">Four</div>
+            <div className="grid-item">Five</div>
+        </div>
+    );
+}
+export default Grid;
