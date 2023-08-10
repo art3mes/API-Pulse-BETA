5722977ae4b7ffcf84b7a6fdac7cc23dbf359c7b
art3mes <93867313+art3mes@users.noreply.github.com>
2023-08-10 21:54:57 +0530

    i

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -4,16 +4,16 @@ import axios from 'axios';
 function WeatherAPI(){
 
     const [cityName, setCityName] = useState("");
+    let weatherInfo={1:'aw'};
 
     function handleChange(event) {
         const newValue = event.target.value;
         setCityName(newValue);
     }
-    let weatherInfo={};
+    
     async function sendData() {
         try {
           const data=await axios.post("http://localhost:3001/", {
-            //await axios.post("https://keeper-app-backend-dyr1.onrender.com", {
             cityName:cityName
           });
           weatherInfo=data.data
@@ -22,19 +22,17 @@ function WeatherAPI(){
           console.error(error);
         }
     }
-
+    function show(){
+        return weatherInfo.temp;
+    }
     return (
-        // <form action="/weather" method="post">
-        //     <label for="cityInput">CITY NAME: </label>
-        //     <input type="text" id="cityInput" name="cityName"/>
-        //     <button type="submit" onClick={sendData()} >GO</button>
-        // </form>
         <div className="form">
             <p>Enter City:</p>
             <input type="text" onChange={handleChange} value={cityName} />
             <button onClick={sendData}>
                 <span>submit</span>
             </button>
+            <p>{show()}</p>
         </div>
     );
 }
