bdf8deaae870324bb23ac6f17464105124eb5be5
art3mes <93867313+art3mes@users.noreply.github.com>
2023-08-15 21:45:23 +0530

    riddle

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,6 +3,7 @@ import WeatherAPI from "./weather";
 import AnimeQuote from "./AnimeQuotes";
 import QRCodeGenerator from "./QRCodeGenerator"
 import QOTD from "./QOTD";
+import Riddle from "./Riddle";
 
 function Grid(){
     return (
@@ -19,7 +20,9 @@ function Grid(){
             <div className="grid-item">
                 <QOTD />
             </div>
-            <div className="grid-item">Five</div>
+            <div className="grid-item">
+                <Riddle />
+            </div>
         </div>
     );
 }
---
+++ src/components/Riddle.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+function QRCodeGenerator(){
+   
+    // async function sendData() {
+    //     try {
+    //       const response=await axios.post("http://localhost:3001/qrcodegenerator", {
+    //         URL:URL
+    //       });
+    //       setQRURL(response.data);
+    //     } catch (error){
+    //       console.error(error);
+    //     }
+    // }
+    return (
+        <div className="">
+            <p>yes</p>
+        </div>
+    );
+}
+
+export default QRCodeGenerator;
