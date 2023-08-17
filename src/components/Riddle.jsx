a17a5315520e8638aebc8cf23b8b148ee2ef2f96
art3mes <93867313+art3mes@users.noreply.github.com>
2023-08-17 19:52:37 +0530

    riddles is set up

--- src/components/Riddle.jsx
+++ src/components/Riddle.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import axios from 'axios';
 
 function QRCodeGenerator(){
+    const [title, setTitle] = useState("");
+    const [question, setQuestion] = useState("");
+    const [answer, setAnswer] = useState("");
    
-    // async function sendData() {
-    //     try {
-    //       const response=await axios.post("http://localhost:3001/qrcodegenerator", {
-    //         URL:URL
-    //       });
-    //       setQRURL(response.data);
-    //     } catch (error){
-    //       console.error(error);
-    //     }
-    // }
+    async function sendData() {
+        try {
+          const response=await axios.post("http://localhost:3001/riddle", {
+          });
+          console.log(response.data[0]);
+          setTitle(response.data[0].title);
+          setQuestion(response.data[0].question);
+          setAnswer(response.data[0].answer);
+        } catch (error){
+          console.error(error);
+        }
+    }
     return (
         <div className="">
-            <p>yes</p>
+            <button onClick={sendData}>
+                <span >Refresh</span>
+            </button>
+            {title && title!=="" ?( 
+                <div>
+                    <p>title: {title} </p>
+                    <p>question: {question}</p>
+                    <p>{answer}</p>
+                </div>
+            ): <p></p>}
         </div>
     );
 }
