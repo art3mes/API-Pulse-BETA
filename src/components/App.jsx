7d34f5ab107faa67c839c5c926227f7ae0895a6d
art3mes <93867313+art3mes@users.noreply.github.com>
2023-08-19 23:04:57 +0530

    background is looking real good

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Grid from "./Grid";
+import Background from "./Background";
 
 function App() {
   
   return (
     <div>
+      <Background />
       <Header />
       <Grid />
       <Footer />
