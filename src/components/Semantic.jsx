97280ddd685c0239e3bda4df122443af676910d2
art3mes <93867313+art3mes@users.noreply.github.com>
2023-09-06 19:16:28 +0530

    working:

--- src/components/Semantic.jsx
+++ src/components/Semantic.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 function Semantic(){
 
     const [text, setText]= useState("");
+    const [score, setScore]=useState("");
+    const [sentiment, setSentiment]=useState("");
+
     function handleChange(event) {
         const newValue = event.target.value;
         setText(newValue);
@@ -14,6 +17,9 @@ function Semantic(){
             text:text
           });
           console.log(response.data);
+          setScore(response.data.score);
+          setSentiment(response.data.sentiment);
+          console.log(sentiment);
         } catch (error){
             console.error(error);
         }
@@ -27,7 +33,16 @@ function Semantic(){
                     <span>Search</span>
                 </button>
             </div>
-            
+            <div className="">
+            {sentiment && sentiment!=="" ?( 
+                <div>
+                    {/* <p>title: {title} </p> */}
+                    <p className=""><strong>Score: </strong>{score}</p>
+                    <p className=""><strong>Text: </strong>{text}</p>
+                    <p className=""><strong>Sentiment: </strong>{sentiment}</p>
+                </div>
+            ): <p></p>}
+        </div>
         </div>
     );
 }
